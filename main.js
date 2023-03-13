1f6fa9f5439418a249d958a01504e9b1319155f4
Rmckinstry <ryanmckinstry2@gmail.com>
2023-03-13 17:16:19 -0500

    adding skeleton modules and factories

---
+++ main.js
@@ -0,0 +1,14 @@
+// gameboard module
+const gameboard = (() =>{
+    let board = ['X','0','X','X','0','X','X','0','X',];
+    return {board};
+})();
+
+const displayController = (()=>{
+    
+})
+
+// player factory
+const playerFactory = (name) =>{
+    return {name}
+}
