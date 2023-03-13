93fbc9fa46cc89fbf4179bc41426bf6d2fe5ff67
Rmckinstry <ryanmckinstry2@gmail.com>
2023-03-13 17:49:27 -0500

    added skeleton board initalize logic to game controller

--- main.js
+++ main.js
@@ -4,11 +4,63 @@ const gameboard = (() =>{
     return {board};
 })();
 
-const displayController = (()=>{
-    
-})
+const gameController = (()=>{
+    function initialize(){
+        let gameContainer = document.getElementById("container")
+        
+        const squareOne = document.createElement('div');
+        squareOne.classList.add("square");
+        squareOne.setAttribute("id", "squareOne")
+
+        const squareTwo = document.createElement('div');
+        squareTwo.classList.add("square");
+        squareTwo.setAttribute("id", "squareTwo")
+
+        const squareThree = document.createElement('div');
+        squareThree.classList.add("square");
+        squareThree.setAttribute("id", "squareThree")
+
+        const squareFour = document.createElement('div');
+        squareFour.classList.add("square");
+        squareFour.setAttribute("id", "squareFour")
+
+        const squareFive = document.createElement('div');
+        squareFive.classList.add("square");
+        squareFive.setAttribute("id", "squareFive")
+
+        const squareSix = document.createElement('div');
+        squareSix.classList.add("square");
+        squareSix.setAttribute("id", "squareSix")
+
+        const squareSeven = document.createElement('div');
+        squareSeven.classList.add("square");
+        squareSeven.setAttribute("id", "squareSeven")
+
+        const squareEight = document.createElement('div');
+        squareEight.classList.add("square");
+        squareEight.setAttribute("id", "squareEight")
+
+        const squareNine = document.createElement('div');
+        squareNine.classList.add("square");
+        squareNine.setAttribute("id", "squareNine")
+
+        gameContainer.appendChild(squareOne);
+        gameContainer.appendChild(squareTwo);
+        gameContainer.appendChild(squareThree);
+        gameContainer.appendChild(squareFour);
+        gameContainer.appendChild(squareFive);
+        gameContainer.appendChild(squareSix);
+        gameContainer.appendChild(squareSeven);
+        gameContainer.appendChild(squareEight);
+        gameContainer.appendChild(squareNine);
+    }
+
+    return{initialize}
+})()
 
 // player factory
 const playerFactory = (name) =>{
     return {name}
 }
+
+gameController.initialize();
